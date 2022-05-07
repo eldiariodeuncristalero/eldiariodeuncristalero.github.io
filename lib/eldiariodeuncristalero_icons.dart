0763815d17c640db328ef2eceda9ee33684f1cee
eldiariodeuncristalero <100989579+eldiariodeuncristalero@users.noreply.github.com>
2022-05-06 23:07:43 -0500

    Add files via upload

---
+++ lib/eldiariodeuncristalero_icons.dart
@@ -0,0 +1,16 @@
+// Place fonts/eldiariodeuncristalero.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: eldiariodeuncristalero
+//      fonts:
+//       - asset: fonts/eldiariodeuncristalero.ttf
+import 'package:flutter/widgets.dart';
+
+class Eldiariodeuncristalero {
+  Eldiariodeuncristalero._();
+
+  static const String _fontFamily = 'eldiariodeuncristalero';
+
+  static const IconData slam = IconData(0xe900, fontFamily: _fontFamily);
+}
